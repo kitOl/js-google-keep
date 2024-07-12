90a2397b8d8e622fbc4192417cf8b70cb04ae4a1
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-07-12 15:25:05 +0300

    add option deleting note

--- js/main.js
+++ js/main.js
@@ -2,19 +2,23 @@ const addNoteForm = document.querySelector("#addNoteForm");
 
 const noteTitleInput = document.querySelector("#noteTitleInput");
 const noteTextInput = document.querySelector("#noteTextInput");
+const noteDelete = document.querySelector("#btnDelete");
 
 const cardsWrapper = document.querySelector("#cardsWrapper");
 
 const notes = [
   {
+    id: 1,
     title: "Basic JS",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
   {
+    id: 2,
     title: "DOM",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
   {
+    id: 3,
     title: "Events in JavaScript",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
@@ -26,6 +30,7 @@ addNoteForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   notes.push({
+    id: notes[notes.length - 1]["id"] + 1,
     title: noteTitleInput.value,
     text: noteTextInput.innerHTML,
   });
@@ -38,9 +43,25 @@ addNoteForm.addEventListener("submit", (e) => {
   formStarter.hidden = false;
   formTextFake.textContent = "Текст заметки...";
   formContent.hidden = true;
-  const lastNote = notes[notes.length - 1];
+
+  const lastIndex = notes.length - 1;
+  const lastNote = notes[lastIndex];
 
   showNote(lastNote);
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.action === "delete") {
+    const id = e.target.dataset.id;
+    const index = notes.findIndex((item) => {
+      return item.id == id;
+    });
+
+    console.log("index", index);
+
+    notes.splice(index, 1);
+    e.target.closest(".card").remove();
+  }
 });
 
 function showAllNotes() {
@@ -56,6 +77,7 @@ function showNote(note) {
   <p class="card-text">
   ${note.text}
   </p>
+  <button data-action="delete" data-id="${note.id}" class="btn btn-dark btn-sm">Удалить заметку</button>
   </div>
   </section>`;
 
